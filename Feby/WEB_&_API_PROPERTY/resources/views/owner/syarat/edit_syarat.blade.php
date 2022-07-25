2e56a4e3a407f475f136f8c4fbd954eb32bbb79c
Feby-Maulana-Hendrayatno <hendrayatno26@gmail.com>
2022-07-25 10:28:45 +0700

    Versi Github Biar Gampang

--- Feby/WEB_&_API_PROPERTY/resources/views/owner/syarat/edit_syarat.blade.php
+++
@@ -1,5 +0,0 @@
-<input type="hidden" name="id" value="{{ $edit->id }}">
-<div class="form-group">
-    <label for="rsyarat"> Syarat </label>
-    <input type="text" class="form-control" name="syarat" placeholder="Masukkan Syarat - Syarat yang diperlukan" value="{{ $edit->syarat }}">
-</div>
